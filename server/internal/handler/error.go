5457a6a826b9e1510c0ba9adbc55c966135392ee
Kasim <qosimmax@gmail.com>
2023-10-16 16:09:00 +0500

    grpc error

--- server/internal/handler/error.go
+++ server/internal/handler/error.go
@@ -6,6 +6,10 @@ import (
 	"errors"
 	"net/http"
 
+	"google.golang.org/grpc/codes"
+
+	"google.golang.org/grpc/status"
+
 	log "github.com/sirupsen/logrus"
 )
 
@@ -25,6 +29,18 @@ func handleError(
 		Error: err.Error(), // Change to something generic if this API is publicly exposed
 	})
 
+	// grpc error check
+	if code, ok := status.FromError(err); ok {
+		errorBody, _ = json.Marshal(struct {
+			Error string `json:"error"`
+		}{
+			Error: code.Message(),
+		})
+
+		statusCode = HTTPStatusFromCode(code.Code())
+
+	}
+
 	var errorCodeWrapper client.ErrorCodeWrapper
 	if errors.As(err, &errorCodeWrapper) {
 		w.Header().Add("X-preserve-error", "1")
@@ -37,5 +53,47 @@ func handleError(
 	}
 
 	w.WriteHeader(statusCode)
-	w.Write(errorBody)
+	_, _ = w.Write(errorBody)
+}
+
+func HTTPStatusFromCode(code codes.Code) int {
+	switch code {
+	case codes.OK:
+		return http.StatusOK
+	case codes.Canceled:
+		return http.StatusRequestTimeout
+	case codes.Unknown:
+		return http.StatusInternalServerError
+	case codes.InvalidArgument:
+		return http.StatusBadRequest
+	case codes.DeadlineExceeded:
+		return http.StatusGatewayTimeout
+	case codes.NotFound:
+		return http.StatusNotFound
+	case codes.AlreadyExists:
+		return http.StatusConflict
+	case codes.PermissionDenied:
+		return http.StatusForbidden
+	case codes.Unauthenticated:
+		return http.StatusUnauthorized
+	case codes.ResourceExhausted:
+		return http.StatusTooManyRequests
+	case codes.FailedPrecondition:
+		// Note, this deliberately doesn't translate to the similarly named '412 Precondition Failed' HTTP response status.
+		return http.StatusBadRequest
+	case codes.Aborted:
+		return http.StatusConflict
+	case codes.OutOfRange:
+		return http.StatusBadRequest
+	case codes.Unimplemented:
+		return http.StatusNotImplemented
+	case codes.Internal:
+		return http.StatusInternalServerError
+	case codes.Unavailable:
+		return http.StatusServiceUnavailable
+	case codes.DataLoss:
+		return http.StatusInternalServerError
+	}
+
+	return http.StatusInternalServerError
 }
